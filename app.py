844b2e380d15eb0a7bbe9253e8ea0536c15f5876
Augusto Noriega <augustoxd2343@gmail.com>
2025-02-14 18:10:47 -0300

    Subiendo V3.1

--- app.py
+++ app.py
@@ -10,25 +10,25 @@ respuesta=""
 continuar=""
 
 def devolverListaOrd(lista,formato):
-#     response = openai.ChatCompletion.create(
-#         model="gpt-4o-mini",
-#         messages=[
-#             {
-#                 "role": "system",
-#                 "content":"Imagina que eres un separador de residuos y que te pasan la lista de residuos o un excel y vos el resultado en el formato que pida el usuario pero filtrada en residuo reciclable que tiene subdivisiones de carton,vidrio,papel y plástico, el residuo orgánico reciclable cascaras de verduras, frutas, semillas, etc y los residuos no reciclables que son curitas, papeles del baño y pañuelos. Sin introduccion ni nada y no respondas hasta que te pasen la lista.Al final de tu respuesta pregunta si quieren que les des consejos de como aprovechar los residuos reciclables solamente si los residuos que te paso son reciclables."
-#             },
-#             {
-#                 "role": "user",
-#                 "content":f"La lista es {lista}. Devolvemelo en el formato {formato}"
-#             }
-#             ]
-# )
-    # st.text(response.choices[0].message["content"])
-    st.text("lista ordenada")
+    response = openai.ChatCompletion.create(
+         model="gpt-4o-mini",
+         messages=[
+             {
+                 "role": "system",
+                 "content":"Imagina que eres un separador de residuos y que te pasan la lista de residuos o un excel y vos el resultado en el formato que pida el usuario pero filtrada en residuo reciclable que tiene subdivisiones de carton,vidrio,papel y plástico, el residuo orgánico reciclable cascaras de verduras, frutas, semillas, etc y los residuos no reciclables que son curitas, papeles del baño y pañuelos. Sin introduccion ni nada y no respondas hasta que te pasen la lista.Al final de tu respuesta pregunta si quieren que les des consejos de como aprovechar los residuos reciclables solamente si los residuos que te paso son reciclables."
+             },
+             {
+                 "role": "user",
+                 "content":f"La lista es {lista}. Devolvemelo en el formato {formato}"
+             }
+             ]
+ )
+    st.text(response.choices[0].message["content"])
     with st.form("Respuesta"):
         st.text("Continuar")
         btnRes=st.form_submit_button("Si")
     if (btnRes):
+        st.text("Nos vemos :D")
         inicio()
 def inicio():
     st.text("Bienvenido a la aplicación de reciclaje")
